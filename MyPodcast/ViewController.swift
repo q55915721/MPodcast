9c0d82dca5e6aadf5c5a3841f455c5c321852a66
Heisenberg <q55915721@gmail.com>
2019-02-11 06:48:04 +0800

    Initial Commit

---
+++ MyPodcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPodcast
+//
+//  Created by 洪森達 on 2019/2/11.
+//  Copyright © 2019 洪森達. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
